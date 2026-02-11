02bbdcc46e91b55db1a96a67a0348d1c1fa5793a
fernandassilva8-jpg <fernanda.s.silva8@ba.estudante.senai.br>
2026-02-11 16:56:00 -0300

    Add files via upload

---
+++ primeira aula no Python.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls")
+
+numero = int(input("Digite seu nome:")
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print ("Antecessor: ", antecessor)
+print ("Sucessor: ", sucessor)
+
